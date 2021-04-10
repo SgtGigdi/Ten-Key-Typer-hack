b8d85e933f368ba149b2d8421db2af16fcc6e765
SgtGigdi <66046963+SgtGigdi@users.noreply.github.com>
2021-04-09 19:02:12 -0500

    Lol

---
+++ stuff/js/script.js
@@ -0,0 +1,1 @@
+$(window).scroll(function(){var scrollTop=$(this).scrollTop();$('#navbar').css({opacity:function(){var elementHeight=$(this).height(),opacity=((1-(elementHeight-scrollTop)/elementHeight)*0.1)+0.5;if(opacity<1){return opacity;}else{return 1;}}});});
